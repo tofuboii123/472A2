7016c7ea7dbf32418e31a5703d39944ef0ca6176
Weiliang Xie <weiliangxie@hotmail.com>
2020-11-07 21:56:43 -0500

    I give up

--- src/UCS.py
+++ src/UCS.py
@@ -1,13 +1,16 @@
 from graph import *
 import queue
 from heapq import *
+from queue import PriorityQueue
 
 class UCS:
 
     def __init__(self, graph):
         self.graph = graph
         self.open_list = []                 
-        self.closed_list = []                      
+        self.closed_list = []
+        self.pq = PriorityQueue()
+        self.nodes = []                      
 
 
     '''
@@ -17,24 +20,28 @@ class UCS:
         print("Searching...")
 
         # Push initial state into PQ
-        heappush(self.open_list, (0, self.graph.start_state, 0))       
+        self.pq.put((0, self.graph.start_state, None))
+        self.open_list.append((0, self.graph.start_state, None))
+        self.nodes.append(self.graph.start_state)
+        # heappush(self.open_list, (0, self.graph.start_state, 0))       
 
         # While there are states in the open list, keep searching
-        while self.open_list:
+        while not self.pq.empty():
 
             # TODO
             # if timeout:
             #   return False
 
             # Remove first element from PQ
-            cost, current_node, parent_node = heappop(self.open_list)
+            cost, current_node, parent_node = self.pq.get()
+            self.open_list.remove((cost, current_node, parent_node))
             self.graph.current_state = current_node
 
             # Debugging
             # print("Total cost: {}, Current Node: {}".format(cost, current_node))
 
             # Visited nodes
-            self.closed_list.append((cost, current_node, parent_node))
+            self.closed_list.append(current_node)
 
             # Check if node is goal
             if self.graph.goal():
@@ -56,19 +63,56 @@ class UCS:
                 add = True
 
                 if not child[1] in self.closed_list:
-                    
-                    # If child is in open list but has a lower cost, remove the higher cost one and add the new child
-                    for i, state in enumerate(self.open_list):
-                        if child[1] == state[1]:
-                            if child[0] < state[0]:
-                                self.open_list.remove(state)
-                                add = True
-                                break
-                            else:
-                                add = False
+                    if not child[1] in self.nodes:
+                        self.nodes.append(child[1])
+                        self.pq.put((child[0] + cost, child[1], child[2]))
+                        self.open_list.append((child[0] + cost, child[1], child[2]))
+                    else:
+                        old_state = [state for state in self.open_list if self.nodes[-1] in state]
+                        if child[0] < old_state[0][0]:
+                            self.nodes.remove(old_state[0][1])
+                            self.open_list.remove(old_state[0])
+                            
+                            old_states = []
+                            
+                            state = self.pq.get()
 
-                    if add:
-                        heappush(self.open_list, (child[0] + cost, child[1], child[2]))
+                            while state[1] != child[1]:
+                                state = self.pq.get()
+                                old_states.append(state)
+
+                            if old_states[-1][0] > child[0]:
+                                old_states[-1] = child
+
+                            for s in old_states:
+                                self.pq.put(s)
+
+                            self.nodes.append(child[1])
+                            self.pq.put((child[0] + cost, child[1], child[2]))
+                            self.open_list.append((child[0] + cost, child[1], child[2]))
+                        
+                        
+                    # repeated_children = [state for state in self.open_list if child[1] in state]
+
+                    # if repeated_children:
+                    #     if repeated_children[0][2] != parent_node:
+                    #          if repeated_children[0][0] < cost:
+
+                    
+                    # # If child is in open list but has a lower cost, remove the higher cost one and add the new child
+                    # for i, state in enumerate(self.open_list):
+                    #     if child[1] == state[1]:
+                    #         if child[0] < state[0]:
+                    #             self.open_list.remove(state)
+                    #             add = True
+                    #             break
+                    #         else:
+                    #             add = False
+
+                    # if add:
+                    #     self.pq.put((child[0] + cost, child[1], child[2]))
+                    #     self.open_list.append((child[0] + cost, child[1], child[2]))
+                    #     # heappush(self.open_list, (child[0] + cost, child[1], child[2]))
 
             print("Open List: ")
             for state in self.open_list:
--- src/test.py
+++ src/test.py
@@ -4,17 +4,18 @@ from UCS import *
 
 p = Puzzle("puzzles/test.txt", (2, 4))
 
-goal_states = [[[1, 2, 3, 4], [5, 6, 7, 0]], [[1, 3, 5, 7], [2, 4, 6, 0]]]
+goal_states = [[1, 2, 3, 4, 5, 6, 7, 0], [1, 3, 5, 7, 2, 4, 6, 0]]
 
 # Easy puzzle for testing
-test_puzzle = [[1, 2, 3, 4], [5, 6, 0, 7]]  
-test_puzzle2 = [[1, 3, 5, 7], [2, 4, 0, 6]] 
-test_puzzle3 = [[1, 2, 3, 4], [5, 0, 6, 7]]
-test_puzzle4 = [[1, 2, 0, 4], [5, 6, 7, 3]]
-test_puzzle5 = [[1, 3, 5, 7], [0, 2, 4, 6]]
+test_puzzle = [1, 2, 3, 4, 5, 6, 0, 7]  
+test_puzzle2 = [1, 3, 5, 7, 2, 4, 0, 6] 
+test_puzzle3 = [1, 2, 3, 4, 5, 0, 6, 7]
+test_puzzle4 = [1, 2, 0, 4, 5, 6, 7, 3]
+test_puzzle5 = [1, 3, 5, 7, 0, 2, 4, 6]
+test_puzzle5 = [0, 1, 2, 4, 3, 5, 6, 7]
 
 
-g1 = Graph(goal_states, test_puzzle4)
+g1 = Graph(goal_states, p.puzzles[0])
 ucs = UCS(g1)
 ucs.search()
 
