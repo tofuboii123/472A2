238f31f2b1f8476c3ab9d7f59897e60e60940c37
tppham94 <43486951+tppham94@users.noreply.github.com>
2020-11-08 12:33:24 -0500

    Update heuristic.py
    Change to fit 1D arrays

--- src/heuristic.py
+++ src/heuristic.py
@@ -13,16 +13,12 @@ class heuristic:
     # Compare distance of position
     def positionCheck(self, arr1, arr2):
         numOfIncorrect = 0
-
         # i=x y=j how far it takes to get there  goalx(end goal) - testx(current)
         for i in range (len(arr1)):
-            for j in range (len(arr1[i])):
-                goalX = np.asarray(np.where(arr1 == arr1[i][j]))[0][0]
-                goalY = np.asarray(np.where(arr1 == arr1[i][j]))[1][0]
-                testX = np.asarray(np.where(arr2 == arr1[i][j]))[0][0]
-                testY = np.asarray(np.where(arr2 == arr1[i][j]))[1][0]
-
-                numOfIncorrect = abs(goalX - testX) + abs(goalY - testY) + numOfIncorrect
+            for j in range (len(arr1)):
+                if(arr1[i] == arr2[j]):
+                    numOfIncorrect = abs(i - j) + numOfIncorrect
+                    break
         return numOfIncorrect
 
     def hammingDistance(self, test_puzzle, goal_states):
@@ -33,18 +29,16 @@ class heuristic:
 
         #Checking the position of each arrays and append the number of error
         for i in range (len(goal_states)): 
-            error = 0
-            for j in range (len(goal_states[i])):
-                error = error + self.compareArr(test_puzzle[j], goal_states[i][j])
+            error = self.compareArr(test_puzzle, goal_states[i])
             errorPerGoalState.append(error)   
 
         #Print out the Goal state with the total error of it 
         for i in range (len(errorPerGoalState)):
             print("Goal State " + str(i) + " : " + str(errorPerGoalState[i]))
 
-    # Calculate the total to arrive to goal state
-    def manhattanDistance(self, test_puzzle, goal_states):
-        print("Manhattan Distance: ")
+    # Alternate version of Manhattan heuristic as 1D states instead of 2D
+    def oneDimensionDistance(self, test_puzzle, goal_states):
+        print("One Dimension Distance: ")
         #Array to get the number of error per goal state
         errorPerGoalState = []
 
@@ -57,8 +51,11 @@ class heuristic:
         for i in range (len(errorPerGoalState)):
             print("Goal State " + str(i) + " : " + str(errorPerGoalState[i]))
         
-test_puzzle = np.array([[1, 2, 3, 4], [5, 6, 0, 7]])  
-goal_states = np.array([[[1, 2, 3, 4], [5, 6, 7, 0]], [[1, 3, 5, 7], [2, 4, 6, 0]]])
+
+# Test Section
+goal_states = [[1, 2, 3, 4, 5, 6, 7, 0], [1, 3, 5, 7, 2, 4, 6, 0]]
+test_puzzle = [1, 2, 3, 4, 5, 6, 0, 7]  
+
 h = heuristic()
 h.hammingDistance(test_puzzle, goal_states)
-h.manhattanDistance(test_puzzle, goal_states)
+h.oneDimensionDistance(test_puzzle, goal_states)
