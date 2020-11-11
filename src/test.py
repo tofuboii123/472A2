987b095295930ec58ce60f40b270f436f8031809
welleh <kevinjiang64@hotmail.com>
2020-11-11 03:11:47 -0500

    gbfs = priority queue

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ from puzzle import *
 from UCS import *
 from heuristic import *
 from a_star import *
+from GBFS import *
 
 p = Puzzle("puzzles/test.txt", (2, 4))
 
@@ -16,14 +17,18 @@ test_puzzle4 = [1, 2, 0, 4, 5, 6, 7, 3]
 # test_puzzle5 = [1, 3, 5, 7, 0, 2, 4, 6]
 test_puzzle5 = [0, 1, 2, 4, 3, 5, 6, 7]
 
-g1 = Graph(goal_states, p.puzzles[1])
+# g1 = Graph(goal_states, p.puzzles[1])
 # ucs = UCS(g1)
 # ucs.search()
 
-aStar = a_star(g1)
+# aStar = a_star(g1)
 # aStar.search(1)
-aStar.search(1)
+# aStar.search(1)
 
 # g2 = Graph(goal_states, test_puzzle2)
 # ucs2 = UCS(g2)
 # ucs2.search()
+
+g3 = Graph(goal_states, p.puzzles[1])
+gbfs = GBFS(g3)
+gbfs.search(1)
