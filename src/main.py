93499487d249eb793caa983670e40c0bd5646fc9
Weiliang Xie <weiliangxie@hotmail.com>
2020-11-17 18:53:53 -0500

    final test

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from GBFS import *
 from utility import *
 
 
-puzzles, goal_states = generatePuzzle(2, 8)
+puzzles, goal_states = generatePuzzle(50, 8)
 writePuzzlesToFile(puzzles)
 
 ucs_values = []
@@ -32,28 +32,28 @@ for i, puzzle in enumerate(puzzles):
     g = Graph(goal_states, len(puzzle), puzzle)
 
     aStar = a_star(g)
-    # aStar2 = a_star(g)
+    aStar2 = a_star(g)
     gbfs = GBFS(g)
-    # gbfs2 = GBFS(g)
+    gbfs2 = GBFS(g)
     ucs = UCS(g)
 
     gbfs.check_timeout(3)
-    # gbfs2.check_timeout(2)
+    gbfs2.check_timeout(2)
     aStar.check_timeout(3)
-    # aStar2.check_timeout(2)
+    aStar2.check_timeout(2)
     ucs.check_timeout()
 
     writeToFile(i, gbfs_h1_name, gbfs)
     writeToFile(i, astar_h1_name, aStar)
     writeToFile(i, ucs_name, ucs)
-    # writeToFile(i, gbfs_h2_name, gbfs2)
-    # writeToFile(i, astar_h2_name, aStar2)
+    writeToFile(i, gbfs_h2_name, gbfs2)
+    writeToFile(i, astar_h2_name, aStar2)
 
     ucs_values.append((stats(i, ucs_name, ucs)))
     gbfs_h1_values.append((stats(i, gbfs_h1_name, gbfs)))
-    # gbfs_h2_values.append((stats(i, gbfs_h2_name, gbfs2)))
+    gbfs_h2_values.append((stats(i, gbfs_h2_name, gbfs2)))
     astar_h1_values.append((stats(i, astar_h1_name, aStar)))
-    # astar_h2_values.append((stats(i, astar_h2_name, aStar2)))
+    astar_h2_values.append((stats(i, astar_h2_name, aStar2)))
 
 print("ucs values:" , ucs_values)
 ucs_stats["avg"] = averageStats(ucs_values)
@@ -73,8 +73,14 @@ print("average gbfs1: ", gbfs_h1_stats["avg"])
 print("total gbfs1: ", gbfs_h1_stats["total"])
 
 
-# print("gbfs2 values: ", gbfs_h2_values)
-# print("average gbfs2: ", averageStats(gbfs_h2_values))
+print("gbfs2 values: ", gbfs_h2_values)
+gbfs_h2_stats["avg"] = averageStats(gbfs_h2_values)
+gbfs_h2_stats["total"] = totalStats(gbfs_h2_values)
+writeStatsToCSV(gbfs_h2_name, gbfs_h2_stats["avg"], gbfs_h2_stats["total"])
+
+
+print("average gbfs2: ", gbfs_h2_stats["avg"])
+print("total gbfs2: ", gbfs_h2_stats["total"])
 
 
 print("astar1 values: ", astar_h1_values)
@@ -82,10 +88,16 @@ astar_h1_stats["avg"] = averageStats(astar_h1_values)
 astar_h1_stats["total"] = totalStats(astar_h1_values)
 writeStatsToCSV(astar_h1_name, astar_h1_stats["avg"], astar_h1_stats["total"])
 
-
 print("average astar1: ", astar_h1_stats["avg"])
 print("total astar1: ", astar_h1_stats["total"])
 
 
-# print("astar2 values: ", astar_h2_values)
-# print("average astar2: ", averageStats(astar_h2_values))
+
+print("astar2 values: ", astar_h2_values)
+astar_h2_stats["avg"] = averageStats(astar_h2_values)
+astar_h2_stats["total"] = totalStats(astar_h2_values)
+writeStatsToCSV(astar_h2_name, astar_h2_stats["avg"], astar_h2_stats["total"])
+
+
+print("average astar2: ", astar_h2_stats["avg"])
+print("total astar2: ", astar_h2_stats["total"])
